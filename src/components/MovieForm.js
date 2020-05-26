8cd355d6bb506594633bc92a189fb975d790c9d8
Benoît Hubert <benoit.hubert@wildcodeschool.fr>
2020-05-26 19:53:27 +0200

    Store all form values in single object

--- src/components/MovieForm.js
+++ src/components/MovieForm.js
@@ -2,20 +2,43 @@ import React, { useState } from 'react';
 import axios from 'axios';
 
 function MovieForm() {
-  const [title, setTitle] = useState('');
-  const [poster, setPoster] = useState('');
-  const [comment, setComment] = useState('');
+  // const [title, setTitle] = useState('');
+  // const [poster, setPoster] = useState('');
+  // const [comment, setComment] = useState('');
+  const [values, setValues] = useState({
+    title: '', comment: '', poster: ''
+  });
 
   const handleSubmit = (event) => {
     event.preventDefault();
-    const movieInfos = { title: title, poster: poster, comment: comment };
     const url = 'https://post-a-form.herokuapp.com/api/movies';
-    axios.post(url, movieInfos)
+    axios.post(url, values)
       .then(res => res.data)
       .then(data => console.log(data))
       .catch(err => console.error(err));
+  };
+
+  const handleChange = (event) => {
+    // 1. De quelles infos j'ai besoin pour modifier l'objet values ?
+    //    -> le "name" et la "value" de l'input (event.target)
+    const { name, value } = event.target;
+    // 2. Avant de mettre à jour values, je "calcule" un nouvel objet
+    // Avant { title: 'Ava', poster: '', comment: '' }
+    // const newValues = { ...values };
+    // newValues[name] = value;
+    const newValues = { ...values, [name]: value };
+
+    // 3. Mise à jour de values
+
+    // a. écriture classique => je passe la nouvelle valeur
+    setValues(newValues);
+
+    // b. écriture "callback"
+    // setValues((oldValues) => ({ ...oldValues, [name]: value }));
 
   };
+
+  const { title, comment, poster } = values;
   return (
     <div className="MovieForm">
       <form onSubmit={handleSubmit}>
@@ -23,7 +46,7 @@ function MovieForm() {
         <input
           id='title'
           name='title'
-          onChange={(event) => setTitle(event.target.value)}
+          onChange={handleChange}
           value={title}
         />
         <label htmlFor='poster'>poster</label>
@@ -31,14 +54,14 @@ function MovieForm() {
           id='poster'
           name='poster'
           type='url'
-          onChange={(event) => setPoster(event.target.value)}
+          onChange={handleChange}
           value={poster}
         />
         <label htmlFor='comment'>comment</label>
         <textarea
           id='comment'
           name='comment'
-          onChange={(event) => setComment(event.target.value)}
+          onChange={handleChange}
           value={comment}
         />
         <button type="submit">Create</button>
